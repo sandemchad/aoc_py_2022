ba5f502fa404e4a3b1bfcbb54e7886f58f772c9d
Chad Sandeman <chadsandeman@gmail.com>
2022-12-03 09:44:27 +1300

    Day 2 completed

--- solutions/day_02.py
+++ solutions/day_02.py
@@ -5,5 +5,40 @@ def read_file() -> str:
 
 if __name__ == "__main__":
   data = read_file()
-  print(f"Part 1: {data}")
-  print(f"Part 2: {data}")
+  pairs = data.split("\n")
+
+  possible_scores = {
+    # wins
+    "C X": 6 + 1,
+    "A Y": 6 + 2,
+    "B Z": 6 + 3,
+    # ties
+    "A X": 3 + 1,
+    "B Y": 3 + 2,
+    "C Z": 3 + 3,
+    # losses
+    "B X": 0 + 1,
+    "C Y": 0 + 2,
+    "A Z": 0 + 3,
+  }
+
+  scores = list(map(possible_scores.get, pairs))
+
+  print(f"Part 1: {sum(scores)}")
+
+  possible_scores = {
+    # losses
+    "A X": 0 + 3,
+    "B X": 0 + 1,
+    "C X": 0 + 2,
+    # ties
+    "A Y": 3 + 1,
+    "B Y": 3 + 2,
+    "C Y": 3 + 3,
+    # win
+    "A Z": 6 + 2,
+    "B Z": 6 + 3,
+    "C Z": 6 + 1,
+  }
+
+  print(f"Part 2: {sum(list(map(possible_scores.get, pairs)))}")
